5b976937005e2c2f96c76a1d42a11c8e68ef94ca
Machenike <624824229@qq.com>
2021-12-16 21:44:27 +0800

    2021.12.16

--- CircleList.h
+++ CircleList.h
@@ -9,7 +9,7 @@ namespace mylib
 	class CircleList :public LinkList<T>
 	{
 	protected:
-		typedef typename LinkList::Node Node;
+		typedef typename LinkList<T>::Node Node;
 		Node* last() const
 		{
 			return this->position(this->m_length-1)->next;
@@ -18,7 +18,7 @@ namespace mylib
 		{
 			last()->next = this->m_header.next;
 		}
-		int mod(int i)
+		int mod(int i) const
 		{
 			return (this->m_length == 0) ? 0 : (i % this->m_length);
 		}
@@ -61,7 +61,7 @@ namespace mylib
 						this->m_header.next = 0;
 						this->m_current = 0;
 					}
-					this->destroy(toDel);//异常安全调用父类函数时要使用this指针
+					this->destroy(toDel);//异常安全调用//父类函数时要使用this指针
 				}
 				else
 				{
@@ -78,14 +78,63 @@ namespace mylib
 		{
 			return LinkList<T>::set(mod(i), e);
 		}
-		T get(i)const
+		T get(int i)const
 		{
 			return LinkList<T>::get(mod(i));
 		}
 		bool get(int i, T& e) const
 		{
-			return LinkList<T>::get(mod(i),e);
+			return LinkList<T>::get(mod(i),e);//mod函数必须为const函数
+		}
+		int find(const T& e)
+		{
+			int ret = -1;
+			Node* slider = this->m_header.next;
+			for (int i = 0; i < this->m_length; i++)
+			{
+				if (slider->value == e)
+				{
+					ret = i;
+					break;
+				}
+				slider = slider->next;
+			}
+			return ret;
+		}
+		void clear()
+		{
+			while (this->m_length > 1)
+			{
+				remove(1);//提高效率所以不需要remove（0）
+			}
+			if (this->m_length == 1)
+			{
+				Node* toDel = this->m_header.next;
+				this->m_header.next = 0;
+				this->m_current = 0;
+				this->m_length = 0;
+				this->destroy(toDel);
+			}
+		}
+		//重新实现遍历操作
+		bool move(int i, int step=1)
+		{
+			return LinkList<T>::move(i, step);
+		}
+		bool end()
+		{
+			return ((this->m_length == 0) || (this->m_current->next == 0));
+		}
+
+		~CircleList()
+		{
+			clear();
 		}
 	};
 }
+/*循环链表的应用
+* 约瑟夫环问题
+* 已知n个人（0~n-1）从编号为k的人开始报数，数到m的人开始出列，下一个人又从1开始数
+* 依次重复直到所有人出列
+*/
 #endif
--- my_lib.cpp
+++ my_lib.cpp
@@ -9,6 +9,7 @@
 #include "LinkList.h"
 #include "StaticLinkList.h"
 #include "SharedPointer.h"
+#include "CircleList.h"
 using namespace mylib;
 using namespace std;
 class Test :public Object
@@ -24,8 +25,26 @@ public:
 		cout << "~Test()" << endl;
 	}
 };
+void func(int n, int s, int m)
+{
+	CircleList<int> list;
+	for (int i = 0; i < m; i++)
+	{
+		list.insert(i + 1);
+	}
+	for (list.move(n-1+s-1,s-1); list.length() > 2;list.next())
+	{
+		cout << list.current() << endl;
+		list.remove(list.find(list.current()));
+	}
+	cout << "————————最终剩余————————" << endl;
+	for (int i = 0; i < list.length(); i++)
+	{
+		cout << list.get(i) << endl;
+	}
+}
 int main()
 {
-	cout << 8% 7 << endl;
+	func(1, 3, 41);
 	return 0;
 }
